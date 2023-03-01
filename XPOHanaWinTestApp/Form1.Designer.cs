e33e2c3b73e15efcefc29ea5006f880c2d1d9455
Leandro Khalil <khalilpdev@gmail.com>
2023-03-01 18:49:52 -0300

    new code

--- XPOHanaWinTestApp/Form1.Designer.cs
+++ XPOHanaWinTestApp/Form1.Designer.cs
@@ -28,63 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtSenhaSap = new System.Windows.Forms.TextBox();
-            this.txtUsuarioSap = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.txtSenhaBanco = new System.Windows.Forms.TextBox();
             this.txtUsuarioBanco = new System.Windows.Forms.TextBox();
-            this.txtCompanyDb = new System.Windows.Forms.TextBox();
-            this.txtServidorLicenca = new System.Windows.Forms.TextBox();
+            this.txtSchema = new System.Windows.Forms.TextBox();
+            this.txtPorta = new System.Windows.Forms.TextBox();
             this.txtServidor = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.cboVersaoSql = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.btnConectar = new System.Windows.Forms.Button();
+            this.btnExecutarSelectAny = new System.Windows.Forms.Button();
+            this.btnSelectFirst = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // txtSenhaSap
-            // 
-            this.txtSenhaSap.Location = new System.Drawing.Point(143, 231);
-            this.txtSenhaSap.Name = "txtSenhaSap";
-            this.txtSenhaSap.PasswordChar = '*';
-            this.txtSenhaSap.Size = new System.Drawing.Size(176, 20);
-            this.txtSenhaSap.TabIndex = 34;
-            this.txtSenhaSap.Text = "mana";
-            // 
-            // txtUsuarioSap
-            // 
-            this.txtUsuarioSap.Location = new System.Drawing.Point(143, 203);
-            this.txtUsuarioSap.Name = "txtUsuarioSap";
-            this.txtUsuarioSap.Size = new System.Drawing.Size(176, 20);
-            this.txtUsuarioSap.TabIndex = 33;
-            this.txtUsuarioSap.Text = "manager";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(26, 234);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(62, 13);
-            this.label7.TabIndex = 32;
-            this.label7.Text = "Senha SAP";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(26, 206);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(67, 13);
-            this.label8.TabIndex = 31;
-            this.label8.Text = "Usuario SAP\r\n";
             // 
             // txtSenhaBanco
             // 
-            this.txtSenhaBanco.Location = new System.Drawing.Point(143, 177);
+            this.txtSenhaBanco.Location = new System.Drawing.Point(143, 153);
             this.txtSenhaBanco.Name = "txtSenhaBanco";
             this.txtSenhaBanco.PasswordChar = '*';
             this.txtSenhaBanco.Size = new System.Drawing.Size(176, 20);
@@ -93,27 +57,27 @@
             // 
             // txtUsuarioBanco
             // 
-            this.txtUsuarioBanco.Location = new System.Drawing.Point(143, 149);
+            this.txtUsuarioBanco.Location = new System.Drawing.Point(143, 125);
             this.txtUsuarioBanco.Name = "txtUsuarioBanco";
             this.txtUsuarioBanco.Size = new System.Drawing.Size(176, 20);
             this.txtUsuarioBanco.TabIndex = 29;
             this.txtUsuarioBanco.Text = "sa";
             // 
-            // txtCompanyDb
+            // txtSchema
             // 
-            this.txtCompanyDb.Location = new System.Drawing.Point(143, 92);
-            this.txtCompanyDb.Name = "txtCompanyDb";
-            this.txtCompanyDb.Size = new System.Drawing.Size(176, 20);
-            this.txtCompanyDb.TabIndex = 28;
-            this.txtCompanyDb.Text = "DEMOBR";
+            this.txtSchema.Location = new System.Drawing.Point(143, 92);
+            this.txtSchema.Name = "txtSchema";
+            this.txtSchema.Size = new System.Drawing.Size(176, 20);
+            this.txtSchema.TabIndex = 28;
+            this.txtSchema.Text = "SYSTEM";
             // 
-            // txtServidorLicenca
+            // txtPorta
             // 
-            this.txtServidorLicenca.Location = new System.Drawing.Point(143, 64);
-            this.txtServidorLicenca.Name = "txtServidorLicenca";
-            this.txtServidorLicenca.Size = new System.Drawing.Size(176, 20);
-            this.txtServidorLicenca.TabIndex = 27;
-            this.txtServidorLicenca.Text = "LEANDROKHALIL:30000";
+            this.txtPorta.Location = new System.Drawing.Point(143, 64);
+            this.txtPorta.Name = "txtPorta";
+            this.txtPorta.Size = new System.Drawing.Size(176, 20);
+            this.txtPorta.TabIndex = 27;
+            this.txtPorta.Text = "39017";
             // 
             // txtServidor
             // 
@@ -121,106 +85,125 @@
             this.txtServidor.Name = "txtServidor";
             this.txtServidor.Size = new System.Drawing.Size(176, 20);
             this.txtServidor.TabIndex = 26;
-            this.txtServidor.Text = "LEANDROKHALIL";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(26, 123);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(79, 13);
-            this.label6.TabIndex = 25;
-            this.label6.Text = "Versão do SQL";
-            // 
-            // cboVersaoSql
-            // 
-            this.cboVersaoSql.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboVersaoSql.FormattingEnabled = true;
-            this.cboVersaoSql.Items.AddRange(new object[] {
-            "2005",
-            "2008",
-            "2012",
-            "2014",
-            "2016",
-            "2017",
-            "2019",
-            "HANA"});
-            this.cboVersaoSql.Location = new System.Drawing.Point(143, 120);
-            this.cboVersaoSql.Name = "cboVersaoSql";
-            this.cboVersaoSql.Size = new System.Drawing.Size(176, 21);
-            this.cboVersaoSql.TabIndex = 24;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(26, 180);
+            this.label5.Location = new System.Drawing.Point(26, 156);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.Size = new System.Drawing.Size(102, 13);
             this.label5.TabIndex = 23;
-            this.label5.Text = "Senha Banco";
+            this.label5.Text = "Database Password";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(26, 152);
+            this.label4.Location = new System.Drawing.Point(26, 128);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.Size = new System.Drawing.Size(78, 13);
             this.label4.TabIndex = 22;
-            this.label4.Text = "Usuario Banco";
+            this.label4.Text = "Database User";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(26, 95);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.Size = new System.Drawing.Size(46, 13);
             this.label3.TabIndex = 21;
-            this.label3.Text = "Banco SAP";
+            this.label3.Text = "Schema";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(26, 67);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.Size = new System.Drawing.Size(26, 13);
             this.label2.TabIndex = 20;
-            this.label2.Text = "Servidor de Licença";
+            this.label2.Text = "Port";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(26, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 19;
-            this.label1.Text = "Servidor";
+            this.label1.Text = "Server";
             // 
             // btnConectar
             // 
             this.btnConectar.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.btnConectar.Location = new System.Drawing.Point(23, 284);
+            this.btnConectar.Location = new System.Drawing.Point(23, 206);
             this.btnConectar.Name = "btnConectar";
             this.btnConectar.Size = new System.Drawing.Size(75, 23);
             this.btnConectar.TabIndex = 18;
             this.btnConectar.Text = "Conectar";
             this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnExecutarSelectAny
+            // 
+            this.btnExecutarSelectAny.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnExecutarSelectAny.Location = new System.Drawing.Point(363, 57);
+            this.btnExecutarSelectAny.Name = "btnExecutarSelectAny";
+            this.btnExecutarSelectAny.Size = new System.Drawing.Size(104, 23);
+            this.btnExecutarSelectAny.TabIndex = 31;
+            this.btnExecutarSelectAny.Text = "Select Any";
+            this.btnExecutarSelectAny.UseVisualStyleBackColor = true;
+            this.btnExecutarSelectAny.Click += new System.EventHandler(this.btnExecutarSelectAny_Click);
+            // 
+            // btnSelectFirst
+            // 
+            this.btnSelectFirst.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnSelectFirst.Location = new System.Drawing.Point(528, 57);
+            this.btnSelectFirst.Name = "btnSelectFirst";
+            this.btnSelectFirst.Size = new System.Drawing.Size(104, 23);
+            this.btnSelectFirst.TabIndex = 34;
+            this.btnSelectFirst.Text = "Select First";
+            this.btnSelectFirst.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(456, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 20);
+            this.textBox1.TabIndex = 36;
+            this.textBox1.Text = "sys.objects";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(360, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Tabela";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(363, 92);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(269, 81);
+            this.txtResult.TabIndex = 37;
+            this.txtResult.Text = "Result";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(644, 328);
-            this.Controls.Add(this.txtSenhaSap);
-            this.Controls.Add(this.txtUsuarioSap);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label8);
+            this.ClientSize = new System.Drawing.Size(644, 241);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSelectFirst);
+            this.Controls.Add(this.btnExecutarSelectAny);
             this.Controls.Add(this.txtSenhaBanco);
             this.Controls.Add(this.txtUsuarioBanco);
-            this.Controls.Add(this.txtCompanyDb);
-            this.Controls.Add(this.txtServidorLicenca);
+            this.Controls.Add(this.txtSchema);
+            this.Controls.Add(this.txtPorta);
             this.Controls.Add(this.txtServidor);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.cboVersaoSql);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -229,31 +212,29 @@
             this.Controls.Add(this.btnConectar);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "XPO HANA Test Connector";
+            this.Text = "XPO SAP HANA Test Connector";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtSenhaSap;
-        private System.Windows.Forms.TextBox txtUsuarioSap;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox txtSenhaBanco;
         private System.Windows.Forms.TextBox txtUsuarioBanco;
-        private System.Windows.Forms.TextBox txtCompanyDb;
-        private System.Windows.Forms.TextBox txtServidorLicenca;
+        private System.Windows.Forms.TextBox txtSchema;
+        private System.Windows.Forms.TextBox txtPorta;
         private System.Windows.Forms.TextBox txtServidor;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox cboVersaoSql;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnExecutarSelectAny;
+        private System.Windows.Forms.Button btnSelectFirst;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtResult;
     }
 }
 
--- XPOHanaWinTestApp/Form1.cs
+++ XPOHanaWinTestApp/Form1.cs
@@ -1,20 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using System;
 using System.Windows.Forms;
 
 namespace XPOHanaWinTestApp
 {
     public partial class Form1 : Form
     {
+        private static Session _session;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_session == null)
+                {
+                    Connect();
+                    btnConectar.Enabled = false;
+                }
+                else
+                    btnConectar.Enabled = true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Connect()
+        {
+            var sqlConn = HanaConnectionProvider.GetConnectionString(
+                txtServidor.Text.Trim(), 
+                Convert.ToInt32(txtPorta.Text.Trim()),
+                txtUsuarioBanco.Text.Trim(),
+                txtSenhaBanco.Text.Trim(),
+                txtSchema.Text.Trim());
+
+            _session = new Session(XpoDefault.GetDataLayer(sqlConn, AutoCreateOption.SchemaAlreadyExists));
+        }
+
+        private void btnExecutarSelectAny_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
